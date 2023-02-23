caf6db097c581bbc68ebba7cf370be931bd8dafc
wishmeheaven <wishmeheaven3@gmail.com>
2023-02-24 01:45:26 +0200

    fixing_navbar_hides_products_(copies_products_styles)

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product'
+import useStyles from './styles';
 
 const products = [
   {
@@ -15,8 +16,12 @@ const products = [
   { id: 2, name: "Macbook", description: "Apple macbook.", price: "$10", image: 'https://s.yimg.com/uu/api/res/1.2/qi8vdW0iSLBJWZUC9fv5Uw--~B/aD0xMzM0O3c9MjAwMDthcHBpZD15dGFjaHlvbg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2020-11/c8aea820-28a0-11eb-9f89-5ddd62987703.cf.jpg' }
 ];
 const Products = () => {
+
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar}/>
       <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
